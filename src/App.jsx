3f4fee648f6ba57bc8d936e4136bea4e7a28e766
Samguy1 <afolabiayoola001@gmail.com>
2024-11-12 12:36:32 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { About_Us, Blog, Home,Product, Services } from "./pages";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <NavBar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="product" element={<Product/>}/>
+        <Route path="services" element={<Services/>}/>
+        <Route path="about-us" element={<About_Us/>}/>
+        <Route path="blog" element={<Blog/>}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
